7208dd621d4b8c57a45991eb27a19c56ae7029cb
Prashant Lalwani <99117462+PrashantLalwani8289@users.noreply.github.com>
2024-05-29 17:46:47 +0530

    Completes the Sign-in and Sign-up form UI

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from '@/components/AuthForm'
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <section className='flex-center size-full max-sm:px-6'>
+        <AuthForm type="sign-in"></AuthForm>
+    </section>
+  )
+}
+
+export default SignIn
